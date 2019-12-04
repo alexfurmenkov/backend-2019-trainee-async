01fa37e24bd9817f74e1b608b3c2e24d03e28c6d
Alexey <alexfurm@mail.ru>
2019-12-04 21:46:09 +0300

    modified audio location

--- src/integrations/speech_to_text.py
+++ src/integrations/speech_to_text.py
@@ -14,35 +14,32 @@ class SpeechToText:
         # Instantiates a client
         client = speech.SpeechClient()
 
-        file_name = os.path.join(os.path.dirname(__file__), audio_path)
+        media_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../media'))
+
+        file_name = os.path.join(media_path, audio_path)
 
         # Loads the audio into memory
-        try:
-            discr_freq = 48000
-            with io.open(file_name, 'rb') as audio_file:
-                content = audio_file.read()
-                content_encoded = base64.b64encode(content)
+        discr_freq = 48000
+        with io.open(file_name, 'rb') as audio_file:
+            content = audio_file.read()
+            content_encoded = base64.b64encode(content)
 
-            config = types.RecognitionConfig(
-                encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
-                sample_rate_hertz=discr_freq,
-                language_code='en-US')
+        config = types.RecognitionConfig(
+            encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+            sample_rate_hertz=discr_freq,
+            language_code='en-US')
 
-            # Detects speech in the audio file
-            content_decoded = base64.b64decode(content_encoded)
-            audio_decoded = types.RecognitionAudio(content=content_decoded)
-            response = client.recognize(config, audio_decoded)
-            phrase = ''
-        except TypeError:
-            return 'Oops. Something went wrong.'
+        # Detects speech in the audio file
+        content_decoded = base64.b64decode(content_encoded)
+        audio_decoded = types.RecognitionAudio(content=content_decoded)
+        response = client.recognize(config, audio_decoded)
+        phrase = ''
 
-        try:
-            for top_results in response.results:
-                for inside_results in top_results.alternatives:
-                    phrase = inside_results.transcript
-            return phrase
-        except AttributeError:
-            return 'Oops. Something went wrong.'
+        for top_results in response.results:
+            for inside_results in top_results.alternatives:
+                phrase = inside_results.transcript
+        return phrase
+
 
 
 
